689af6428bbac73d74406f706a8bd413fd8fd6ec
s13002 <s13002@std.it-college.ac.jp>
2015-10-19 11:03:29 +0900

    演習の追加

---
+++ Q0201.java
@@ -0,0 +1,9 @@
+class test0201 {
+  static int fact(int n){
+    return n == 0 ? 1 : n * fact(n - 1);
+  }
+
+  public static void main(String[] args){
+	System.out.println("10の階乗は " + fact(10));
+  }
+}                        
---
+++ Q0202.java
@@ -0,0 +1,12 @@
+class test0202 {
+  public static void main(String[] args){
+	double a = 5.5;
+	double b = 10.0;
+	double c = 12.1;
+
+	System.out.println((a+b+c) / 3);
+              
+
+    }
+  }
+
